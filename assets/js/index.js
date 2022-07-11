d25c6eae0ed39e467f804bae5bfa3d9fc715ff7b
Nat Epstein <nathaniel.epstein@gmail.com>
2022-07-11 15:03:45 -0400

    added html elements and started questions object

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+/* first right questions object this will have the question, the choices 
+(an array of possible answers), and the actual answer
+*/
+
+const questions = [
+    {
+        questionText: "Who was the first actor to win Best Actor at the Academy Awards?",
+        choices: ["Emil Jannings", "Humphrey Bogart", "John Travolta", "Peter Lorre"],
+        answer: "Emil Jannings"
+    },
+    {
+        
+    },
+    {
+
+    }
+]
