34f1b5c712121454cbd650cd8768f5fe5dcca7fb
Nat Epstein <nathaniel.epstein@gmail.com>
2022-07-14 15:13:40 -0400

    values now read as the choice text

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,12 @@ const questions = [
     {
         questionText: "Who was the first actor to win Best Actor at the Academy Awards?",
         choices: ["Emil Jannings", "Humphrey Bogart", "John Travolta", "Peter Lorre"],
-        answer: "Emil Jannings",
+        answer: "Emil Jannings"
     },
     {
         questionText: 'Who first refused the Best Actor award at the Oscars?',
         choices: ["Marlon Brando", "Peter Fonda", "George C. Scott", "Will Smith"],
-        answer: "George C. Scott",
+        answer: "George C. Scott"
     },
     {
         questionText: 'Who directed Scarface (1983)?',
@@ -57,7 +57,7 @@ function askQuestions() {
     let choiceArray = currentQuestion.choices;
     for (let i = 0; i < choiceArray.length; i++) {
         let choiceButtonEl = document.createElement('button');
-        choiceButtonEl.textContent = (i + 1) + ". " + choiceArray[i];
+        choiceButtonEl.textContent = choiceArray[i];
         choicesEl.appendChild(choiceButtonEl);
     }
 }
@@ -71,7 +71,51 @@ function runClock() {
     }
 }
 
+function handleChoices(event) {
+    let choiceValue = event.target.textContent 
+    // console.log(event.target);
+    console.log(choiceValue);
+    if (choiceValue === questions[quizQuestionIndex].answer) {
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = 'green';
+        showFunnyImage('correct');
+        playSound('correct');
+    } else {
+        timeCount -= 5;
+        if (timeCount < 0) {
+            timeCount = 0
+        }
+        timerEl.textContent = timeCount;
+        feedbackEl.textContent = "Unfortunately, you are wrong!";
+        feedbackEl.style.color = 'red';
+        showFunnyImage('poop')
+        playSound('wrong')
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function () {
+        feedbackEl.setAttribute('class', 'hidden');
+    }, 1000);
 
+    quizQuestionIndex++;
+    if (quizQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        askQuestions();
+    }
+}
+
+function quizEnd() {
+    //console.log('quizEnd');
+    // clear time interval
+    clearInterval(timerID);
+    // remove the hide settings set in css for the finish screen
+    finishScreenEl.removeAttribute("class");
+    finalScoreEl.textContent = timeCount;
+    // make sure to hide the questions element. You have to set the attribute to hide which in the css says it will display no content
+    questionEl.setAttribute("class", "hide");
+    return;
+
+}
 
 
 function playSound(name) {
@@ -84,12 +128,13 @@ function showFunnyImage(name) {
     funnyImage.src = 'assets/images/' + name + '.jpeg'
     document.body.appendChild(funnyImage);
     setTimeout(function() {
-        funnyImage.classList.add('hide')
+        funnyImage.classList.add('hidden')
     }, 1000)
 }
 
 
 
 
-// Event Handlers
+// Event Listeners
 startButton.addEventListener('click', startQuiz);
+choicesEl.onclick = handleChoices;
